681ae86a233c59b1789de457e445efe639a70297
Thomas Leese <thomas@leese.io>
2024-05-16 11:03:07 +0100

    Create Xcode project

---
+++ Windows/WindowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindowsApp.swift
+//  Windows
+//
+//  Created by Thomas Leese on 16/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct WindowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
